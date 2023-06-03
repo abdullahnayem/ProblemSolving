e40808779ad8b702986aead9dc9b4666426274da
Abdullah Md.Nayem <nayem14@cse.pstu.ac.bd>
2023-06-03 17:03:11 +0600

    Highest Number from ArrayList

---
+++ FindHighest_LowestNumberFromArray/FindHighest_LowestNumberFromArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FindHighest_LowestNumberFromArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); 
+            int[] ArrayList = { 2, -4, 5, 2 };
+            int HighestVal = FindHighest_LowestNumber(ArrayList);
+            Console.WriteLine(HighestVal);
+
+        }
+
+        static int FindHighest_LowestNumber(int[] ArrayList)
+        {
+            int HighestValue = ArrayList[0];
+
+            for (int i = 0; i < ArrayList.Length; i++)
+            {
+                if (HighestValue < ArrayList[i])
+                {
+                    HighestValue = ArrayList[i];
+                }
+            }
+            return HighestValue;
+        }
+
+
+    }
+}
